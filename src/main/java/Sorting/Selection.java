0b6eb23e8cd8528f7467ba1a1e73985a637217f9
Feida <misterfeida@hotmail.com>
2021-03-21 15:36:05 +0100

    feat: add selection sort

---
+++ src/main/java/Sorting/Selection.java
@@ -0,0 +1,20 @@
+package Sorting;
+
+public class Selection {
+    public int[] sort(int[] unsortedArray) {
+        int[] sortedArray;
+        for (int i = 0; i < unsortedArray.length - 1; i++) {
+            int minimumIndex = i;
+            for (int j = i + 1; j < unsortedArray.length; j++) {
+                if (unsortedArray[j] < unsortedArray[minimumIndex]) {
+                    minimumIndex = j;
+                }
+            }
+            int temp = unsortedArray[minimumIndex];
+            unsortedArray[minimumIndex] = unsortedArray[i];
+            unsortedArray[i] = temp;
+        }
+        sortedArray = unsortedArray;
+        return sortedArray;
+    }
+}
