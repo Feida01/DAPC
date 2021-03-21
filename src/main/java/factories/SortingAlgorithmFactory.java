b0ffcb25877994adc2642636188414e6d614e964
Feida <misterfeida@hotmail.com>
2021-03-21 21:03:14 +0100

    refactor: put hardcoded strings into variables

--- src/main/java/factories/SortingAlgorithmFactory.java
+++ src/main/java/factories/SortingAlgorithmFactory.java
@@ -1,20 +1,26 @@
 package factories;
 
+import sorting.Bubble;
 import sorting.Insertion;
 import sorting.Selection;
 import sorting.SortingAlgorithm;
 
 public class SortingAlgorithmFactory {
+    final String SELECTION_SORT = "SELECTION";
+    final String INSERTION_SORT = "INSERTION";
+    final String BUBBLE_SORT = "BUBBLE";
 
     public SortingAlgorithm getSortingAlgorithm(String sortingAlgorithmName) {
         if (sortingAlgorithmName == null) {
             return null;
         }
 
-        if (sortingAlgorithmName.equalsIgnoreCase("SELECTION")) {
+        if (sortingAlgorithmName.equalsIgnoreCase(SELECTION_SORT)) {
             return new Selection();
-        } else if (sortingAlgorithmName.equalsIgnoreCase("INSERTION")) {
+        } else if (sortingAlgorithmName.equalsIgnoreCase(INSERTION_SORT)) {
             return new Insertion();
+        } else if (sortingAlgorithmName.equalsIgnoreCase(BUBBLE_SORT)) {
+            return new Bubble();
         }
 
         return null;
