797fb2c6772b86c81af266486cf0b8799f2ff06c
Feida <misterfeida@hotmail.com>
2021-03-21 15:35:54 +0100

    feat: add insertion sort

---
+++ src/main/java/Sorting/Insertion.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+public class Insertion {
+    public int[] sort(int[] unsortedArray) {
+        int[] sortedArray;
+        for (int i = 1; i < unsortedArray.length; i++) {
+            int temp = unsortedArray[i];
+            int j = i - 1;
+            while (j >= 0 && temp < unsortedArray[j]) {
+                unsortedArray[j + 1] = unsortedArray[j];
+                j = j - 1;
+            }
+            unsortedArray[j + 1] = temp;
+        }
+        sortedArray = unsortedArray;
+        return sortedArray;
+    }
+}
